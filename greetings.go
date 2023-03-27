7f0393e6f8184d80a247f19ab94d5b390714c53a
goldingp <6238669+goldingp@users.noreply.github.com>
2023-03-26 21:16:35 -0600

    Add Hellos to greet multiple people

--- greetings.go
+++ greetings.go
@@ -18,6 +18,24 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+// Hellos returns a map that associates each of the named people
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, helloErr := Hello(name)
+
+		if helloErr != nil {
+			return nil, helloErr
+		}
+
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 // init sets initial values for variables used in the function
 func init() {
 	rand.Seed(time.Now().UnixNano())
